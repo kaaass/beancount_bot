5b629b0174bb0908accb9255d809776b12cf2399
KAAAsS <admin@kaaass.net>
2021-09-30 21:35:59 +0800

    实现首次使用机器人的鉴权

--- beancount_bot/bot.py
+++ beancount_bot/bot.py
@@ -1,28 +1,106 @@
 import telebot
 from telebot import apihelper
-from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, MessageEntity, Message
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton, MessageEntity, Message, CallbackQuery
 
 from beancount_bot import transaction, logger
 from beancount_bot.config import get_config, load_config
+from beancount_bot.session import get_session, SESS_AUTH, get_session_for, set_session
 from beancount_bot.transaction import get_manager
+
+apihelper.ENABLE_MIDDLEWARE = True
 
 bot = telebot.TeleBot(token=None, parse_mode=None)
 
 
+@bot.middleware_handler(update_types=['message'])
+def session_middleware(bot_instance, message):
+    """
+    会话中间件
+    :param bot_instance:
+    :param message:
+    :return:
+    """
+    bot_instance.session = get_session_for(message.from_user.id)
+
+
+#######
+# 鉴权 #
+#######
+
+def check_auth() -> bool:
+    """
+    检查是否登录
+    :return:
+    """
+    return SESS_AUTH in bot.session and bot.session[SESS_AUTH]
+
+
+@bot.message_handler(commands=['start'])
+def start_handler(message: Message):
+    """
+    首次聊天时鉴权
+    :param message:
+    :return:
+    """
+    auth = get_session(message.from_user.id, SESS_AUTH, False)
+    if auth:
+        bot.reply_to(message, "已经鉴权过了！")
+        return
+    # 要求鉴权
+    bot.reply_to(message, "欢迎使用记账机器人！请输入鉴权令牌：")
+
+
+def auth_token_handler(message: Message):
+    """
+    登陆令牌回调
+    :param message:
+    :return:
+    """
+    if check_auth():
+        return
+    # 未鉴权则视为鉴权令牌
+    auth_token = get_config('bot.auth_token')
+    if auth_token == message.text:
+        set_session(message.from_user.id, SESS_AUTH, True)
+        bot.reply_to(message, "鉴权成功！")
+    else:
+        bot.reply_to(message, "鉴权令牌错误！")
+
+
+#######
+# 指令 #
+#######
+
+
 @bot.message_handler(commands=['reload'])
 def reload_handler(message):
+    if not check_auth():
+        bot.reply_to(message, "请先进行鉴权！")
+        return
     load_config()
     bot.reply_to(message, "成功重载配置！")
 
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['help'])
 def send_welcome(message):
+    if not check_auth():
+        bot.reply_to(message, "请先进行鉴权！")
+        return
     # TODO
     bot.reply_to(message, "Howdy, how are you doing?")
 
 
+#######
+# 交易 #
+#######
+
+
 @bot.message_handler(func=lambda m: True)
 def transaction_query_handler(message: Message):
+    if not check_auth():
+        auth_token_handler(message)
+        return
+    # 已鉴权则处理交易
     manager = get_manager()
     try:
         tx_uuid, tx = manager.create_from_str(message.text)
@@ -40,12 +118,16 @@ def transaction_query_handler(message: Message):
 
 
 @bot.callback_query_handler(func=lambda call: call.data[:8] == 'withdraw')
-def callback_withdraw(call):
+def callback_withdraw(call: CallbackQuery):
     """
     交易撤回回调
     :param call:
     :return:
     """
+    auth = get_session(call.from_user.id, SESS_AUTH, False)
+    if not auth:
+        bot.answer_callback_query(call.id, '请先进行鉴权！')
+        return
     tx_uuid = call.data[9:]
     manager = get_manager()
     try:
